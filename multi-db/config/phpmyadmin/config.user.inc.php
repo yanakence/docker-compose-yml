f521049cec5effd14cf7f2fec8345bee4fc31a47
Kensuke Yanai <yanakence@yanah.jp>
2022-05-22 14:06:01 +0900

    fix: mysql version on the label

--- multi-db/config/phpmyadmin/config.user.inc.php
+++ multi-db/config/phpmyadmin/config.user.inc.php
@@ -4,7 +4,7 @@ $i = 0;
 
 $i++;
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
-$cfg['Servers'][$i]['verbose'] = 'MySQL 5.7.37';
+$cfg['Servers'][$i]['verbose'] = 'MySQL 5.7.38';
 $cfg['Servers'][$i]['host'] = 'db1';
 $cfg['Servers'][$i]['port'] = '3306';
 $cfg['Servers'][$i]['compress'] = false;
@@ -12,7 +12,7 @@ $cfg['Servers'][$i]['AllowNoPassword'] = false;
 
 $i++;
 $cfg['Servers'][$i]['auth_type'] = 'cookie';
-$cfg['Servers'][$i]['verbose'] = 'MySQL 8.0.28';
+$cfg['Servers'][$i]['verbose'] = 'MySQL 8.0.29';
 $cfg['Servers'][$i]['host'] = 'db2';
 $cfg['Servers'][$i]['port'] = '3306';
 $cfg['Servers'][$i]['compress'] = false;
